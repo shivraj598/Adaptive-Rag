20f3ba9672230415db7b20db8bdae663537f1f4d
shivraj.dev <timilsenashivraj598@gmail.com>
2026-07-20 18:43:25 +0545

    Refactor routing_tool: simplify return logic by removing unused routes for improved clarity

--- src/tools/graph_tools.py
+++ src/tools/graph_tools.py
@@ -7,10 +7,7 @@ from typing import Literal
 from src.models.state import State
 
 
-def routing_tool(state: State) -> Literal["__end__", "general_llm", "web_search"]:
-    if state["route"] == "index":
-        return "__end__"
-    elif state["route"] == "general":
-        return "general_llm"
-    else:
+def routing_tool(state: State) -> Literal["__end__", "web_search"]:
+    if state["route"] == "search":
         return "web_search"
+    return "__end__"
